93e9f2e62b7b6a7550e28634f7eee3bdd5fdfa8b
Moch Rizal Herdiansyah <mochrizalherdiansyah0211@gmail.com>
2024-06-06 00:38:25 +0700

    buat file di config

---
+++ backend/config/database.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("MySQL connected");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = { sequelize, connectDB };
