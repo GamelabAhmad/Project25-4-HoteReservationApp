e0289ebbee70fec02181102a1969e0cfdf33bb19
septinna <129613418+septinna@users.noreply.github.com>
2024-06-07 21:35:57 +0700

    ngedit room

--- frontend/src/component/StyledRoom.js
+++ frontend/src/component/StyledRoom.js
@@ -5,18 +5,42 @@ export const AboutDiv = styled.h1`
     font-family: 'Times New Roman', Times, serif;
     text-align: center;
     font-size: 50px;
+
+    @media (max-width: 768px) {
+        font-size: 40px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 30px;
+    }
 `;
 
 export const AboutStyle = styled.h1`
     font-family: 'Times New Roman', Times, serif;
     text-align: center;
     font-size: 50px;
+
+    @media (max-width: 768px) {
+        font-size: 40px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 30px;
+    }
 `;
 
 export const AboutHome = styled.p`
     font-family: 'Arial';
     text-align: center;
     font-size: 18px;
+
+    @media (max-width: 768px) {
+        font-size: 16px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 14px;
+    }
 `;
 
 export const AboutRoom = styled.p`
@@ -25,27 +49,48 @@ export const AboutRoom = styled.p`
     justify-content: left;
     background-color: white;
     padding-bottom: 15px;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        text-align: center;
+    }
 `;
 
-// Styling for the image container
 export const AboutImg = styled.div`
     width: 60%;
     box-sizing: border-box;
     text-align: left;
+
     img {
         width: 350px;
-        height: 250px
+        height: 250px;
+
+        @media (max-width: 768px) {
+            width: 100%;
+            height: auto;
+        }
+    }
+
+    @media (max-width: 768px) {
+        width: 100%;
+        text-align: center;
     }
 `;
 
-// Styling for the description container
 export const AboutPesan = styled.div`
     padding-left: 35px;
     text-align: left;
     width: 40%;
     box-sizing: border-box;
+
     h1 {
         font-size: 20px;
+    }
+
+    @media (max-width: 768px) {
+        width: 100%;
+        padding-left: 0;
+        text-align: center;
     }
 `;
 
@@ -53,6 +98,14 @@ export const AboutJudul = styled.h2`
     font-size: 35px;
     font-family: 'Times New Roman', Times, serif;
     padding-bottom: 10px;
+
+    @media (max-width: 768px) {
+        font-size: 30px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 25px;
+    }
 `;
 
 export const AboutDeskripsi = styled.div`
@@ -79,6 +132,37 @@ export const AboutBody = styled.p`
         padding-right: 225px;
         padding-left: 225px;
     }
+
+    @media (max-width: 768px) {
+        font-size: 18px;
+        padding-right: 25px;
+        padding-left: 25px;
+
+        h1 {
+            font-size: 50px;
+        }
+
+        p {
+            padding-right: 25px;
+            padding-left: 25px;
+        }
+    }
+
+    @media (max-width: 480px) {
+        font-size: 16px;
+        padding-right: 10px;
+        padding-left: 10px;
+
+        h1 {
+            font-size: 40px;
+        }
+
+        p {
+            font-size: 14px;
+            padding-right: 10px;
+            padding-left: 10px;
+        }
+    }
 `;
 
 export const Input = styled.input`
@@ -88,6 +172,14 @@ export const Input = styled.input`
     border: 1px solid #ccc;
     border-radius: 4px;
     box-sizing: border-box;
+
+    @media (max-width: 768px) {
+        width: 80%;
+    }
+
+    @media (max-width: 480px) {
+        width: 100%;
+    }
 `;
 
 export const Select = styled.select`
@@ -97,8 +189,15 @@ export const Select = styled.select`
     border: 1px solid #ccc;
     border-radius: 4px;
     box-sizing: border-box;
-`;
 
+    @media (max-width: 768px) {
+        width: 80%;
+    }
+
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+`;
 
 export const Button = styled.button`
     padding: 10px 40px;
@@ -108,37 +207,76 @@ export const Button = styled.button`
     border-radius: 4px;
     cursor: pointer;
     font-size: 16px;
+
     &:hover {
         background-color: black;
         color: white;
     }
+
+    @media (max-width: 768px) {
+        padding: 10px 30px;
+        font-size: 14px;
+    }
+
+    @media (max-width: 480px) {
+        padding: 10px 20px;
+        font-size: 12px;
+    }
 `;
 
 export const Price = styled.h2`
-    font-size: 24px;
-    color: black; /* warna lainnya disesuaikan */
+    font-size: 24px; /* Mengatur font size default */
+    color: black;
+
+    @media (max-width: 768px) {
+        font-size: 20px; /* Mengatur font size pada layar dengan lebar maksimum 768px */
+    }
+
+    @media (max-width: 480px) {
+        font-size: 18px; /* Mengatur font size pada layar dengan lebar maksimum 480px */
+    }
 `;
 
 export const DollarSign = styled.span`
     color: orange;
-    font-size: 50px;
+    font-size: 50px; /* Mengatur font size default */
+
+    @media (max-width: 768px) {
+        font-size: 40px; /* Mengatur font size pada layar dengan lebar maksimum 768px */
+    }
+
+    @media (max-width: 480px) {
+        font-size: 30px; /* Mengatur font size pada layar dengan lebar maksimum 480px */
+    }
 `;
 
 export const Keterangan = styled.div`
     display: flex;
     justify-content: space-between;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
 
 export const Kanan = styled.div`
     width: 50%;
     font-size: 14px;
     color: #666;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 export const Kiri = styled.div`
     width: 50%;
     font-size: 14px;
     color: #666;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 export const DeskripsiLengkap = styled.p`
@@ -146,6 +284,14 @@ export const DeskripsiLengkap = styled.p`
     font-size: 14px;
     color: #666;
     line-height: 1.6;
+
+    @media (max-width: 768px) {
+        font-size: 12px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 10px;
+    }
 `;
 
 export const Deskripsi = styled.div`
@@ -154,6 +300,14 @@ export const Deskripsi = styled.div`
     border-radius: 8px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     margin-bottom: 20px;
+
+    @media (max-width: 768px) {
+        padding-left: 10px;
+    }
+
+    @media (max-width: 480px) {
+        padding-left: 5px;
+    }
 `;
 
 export const Kamar = styled.div`
@@ -164,6 +318,14 @@ export const Kamar = styled.div`
     padding-bottom: 15px;
     width: 60%;
     text-align: left;
+
+    @media (max-width: 768px) {
+        width: 80%;
+    }
+
+    @media (max-width: 480px) {
+        width: 100%;
+    }
 `;
 
 export const Judul = styled.h3`
@@ -171,13 +333,20 @@ export const Judul = styled.h3`
     align-items: left;
     font-size: 20px;
     color: #333;
+
+    @media (max-width: 768px) {
+        font-size: 18px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 16px;
+    }
 `;
 
 export const Bintang = styled.div`
     width: 20%;
     align-items: center;
 `;
-
 
 export const ButtonBook = styled.button`
     width: 20%;
@@ -188,9 +357,20 @@ export const ButtonBook = styled.button`
     border-radius: 4px;
     cursor: pointer;
     font-size: 16px;
+
     &:hover {
         background-color: white;
         color: #b86614;
         border: 1px solid #b86614;
+    }
+
+    @media (max-width: 768px) {
+        width: 30%;
+        font-size: 14px;
+    }
+
+    @media (max-width: 480px) {
+        width: 40%;
+        font-size: 12px;
     }
 `;
--- frontend/src/page/Room.js
+++ frontend/src/page/Room.js
@@ -16,50 +16,48 @@ import '../Room.css';
 
 const Room = () => {
   const rooms = [
-    { id: 1, name: 'Premium King Room', image: Kamar1, price: '159$', size: '30 ft', capacity: 'Max person 3', bed: 'King Beds', services: 'Wifi, Television, Bathroom' },
-    { id: 2, name: 'Deluxe Room', image: Kamar2, price: '159$', size: '30 ft', capacity: 'Max person 5', bed: 'King Beds', services: 'Wifi, Television, Bathroom' },
-    { id: 3, name: 'Luxury Room', image: Kamar3, price: '159$', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Television, Bathroom' },
-    { id: 4, name: 'Double Room', image: Kamar4, price: '159$', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Television, Bathroom' },
-    { id: 5, name: 'Room With View', image: Kamar5, price: '159$', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Television, Bathroom' },
-    { id: 6, name: 'Room With View', image: Kamar6, price: '159$', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Television, Bathroom' }
+    { id: 1, name: 'Premium King Room', image: Kamar1, price: 'IDR 2.300.000', size: '30 ft', capacity: 'Max person 3', bed: 'King Beds', services: 'Wifi, Televisi, Kamar Mandi' },
+    { id: 2, name: 'Deluxe Room', image: Kamar2, price: 'IDR 2.500.000', size: '30 ft', capacity: 'Max person 5', bed: 'King Beds', services: 'Wifi, Televisi, Kamar Mandi' },
+    { id: 3, name: 'Luxury Room', image: Kamar3, price: 'IDR 2.000.000', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Televisi, Kamar Mandi' },
+    { id: 4, name: 'Double Room', image: Kamar4, price: 'IDR 1.800.000', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Televisi, Kamar Mandi' },
+    { id: 5, name: 'Room With View', image: Kamar5, price: 'IDR 2.200.000', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Televisi, Kamar Mandi' },
+    { id: 6, name: 'Room With View', image: Kamar6, price: 'IDR 2.200.000', size: '30 ft', capacity: 'Max person 1', bed: 'King Beds', services: 'Wifi, Televisi, Kamar Mandi' }
   ];
 
   return (
     <div>
       <AboutDiv>
-        <AboutStyle>Our Room</AboutStyle>
-        <AboutHome>
-          Rooms 
-        </AboutHome>
+        <AboutStyle>Kamar Kami</AboutStyle>
+        <AboutHome>Kamar</AboutHome>
       </AboutDiv>
       <div className="about-container">
         <div className="about-body">
           {rooms.map(room => (
             <div className="box" key={room.id}>
               <AboutImg>
-                <img src={room.image} alt="Logo Hotel" />
+                <img src={room.image} alt="Gambar Kamar" />
               </AboutImg>
-              <h3>{room.name}</h3>
+              <h3><b>{room.name}</b></h3>
               <span className="price">
                 <span className="amount">{room.price}</span>
-                <span className="pernight">/Pernight</span>
+                <span className="pernight">/Per Malam</span>
               </span>
-              <span className="deskripsi">
-                <span className="kiri">
-                  <p>Size :</p>
-                  <p>Capacity :</p>
-                  <p>Bed :</p>
-                  <p>Services :</p>
-                </span>
-                <span className="kanan">
+              <div className="deskripsi">
+                <div className="kiri">
+                  <p>Ukuran :</p>
+                  <p>Kapasitas :</p>
+                  <p>Tempat Tidur :</p>
+                  <p>Layanan :</p>
+                </div>
+                <div className="kanan">
                   <p>{room.size}</p>
                   <p>{room.capacity}</p>
                   <p>{room.bed}</p>
                   <p>{room.services}</p>
-                </span>
-              </span>
+                </div>
+              </div>
               <button className="more-detail-button">
-                <b><Link to={`/detail-kamar/${room.id}`} style={{ textDecoration: 'none', color: 'black' }}>More Details</Link></b>
+                <b><Link to={`/detail-kamar/${room.id}`} style={{ textDecoration: 'none', color: 'black' }}>Baca Selengkapnya</Link></b>
               </button>
             </div>
           ))}
