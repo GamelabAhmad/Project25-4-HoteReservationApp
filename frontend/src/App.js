446785ba703018b038031d80c15361a19c8d594d
Moch Rizal Herdiansyah <mochrizalherdiansyah0211@gmail.com>
2024-06-09 22:54:31 +0700

    update validasi login dengan database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,43 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Header from './page/Header';
-import Footer from './page/Footer';
-import Kontak from './page/Kontak';
-import Tentang from './page/Tentang';
-import Body from './page/Body';
-import Room from './page/Room';
-import Login from './page/Login';
-import Daftar from './page/Daftar';
-import DetailRoom from './page/DetailRoom';
-import Pesanan from './page/Pesanan';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './css/bootstrap.min.css';
-import './css/animate.css';
-import axios from 'axios';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Header from "./page/Header";
+import Footer from "./page/Footer";
+import Kontak from "./page/Kontak";
+import Tentang from "./page/Tentang";
+import Body from "./page/Body";
+import Room from "./page/Room";
+import Login from "./page/Login";
+import Daftar from "./page/Daftar";
+import DetailRoom from "./page/DetailRoom";
+import Pesanan from "./page/Pesanan";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./css/bootstrap.min.css";
+import "./css/animate.css";
+import axios from "axios";
 const App = () => {
   const location = useLocation();
-  const noHeaderFooterPaths = ['/masuk', '/daftar'];
-  const shouldHideHeaderFooter = noHeaderFooterPaths.includes(location.pathname);
+  const noHeaderFooterPaths = ["/masuk", "/daftar"];
+  const shouldHideHeaderFooter = noHeaderFooterPaths.includes(
+    location.pathname
+  );
 
   return (
     <div className="App">
       {!shouldHideHeaderFooter && <Header />}
       <Routes>
-        <Route path="/" element={<Body />} />
+        <Route path="/body" element={<Body />} />
         <Route path="/tentang" element={<Tentang />} />
         <Route path="/ruang" element={<Room />} />
         <Route path="/kontak" element={<Kontak />} />
         <Route path="/pesanan" element={<Pesanan />} />
-        <Route path="/detail-kamar/:roomId" element={<DetailRoom />} /> 
+        <Route path="/detail-kamar/:roomId" element={<DetailRoom />} />
         <Route path="/masuk" element={<Login />} />
         <Route path="/daftar" element={<Daftar />} />
       </Routes>
